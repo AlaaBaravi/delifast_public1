c36dfe5d2783c6e6c9ad42dea4f81e742f557b03
AlaaBaravi <97338784+AlaaBaravi@users.noreply.github.com>
2026-06-02 13:41:13 +0300

    Update webhooks.server.js

--- delifast/app/webhooks.server.js
+++ delifast/app/webhooks.server.js
@@ -6,8 +6,8 @@ import prisma from "./db.server";
 
 export const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
-  apiSecretKey: process.env.SHOPIFY_API_SECRET,
-  apiVersion: LATEST_API_VERSION,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET_KEY,
+  apiVersion: process.env.SHOPIFY_API_VERSION || LATEST_API_VERSION,
   scopes: process.env.SCOPES ? process.env.SCOPES.split(",") : [],
   appUrl: process.env.SHOPIFY_APP_URL,
   authPathPrefix: "/auth",
